7e115f1ca647f973c3ff3e984f2cc5ae9eb89136
cqc1994 <1132166212@qq.com>
2021-05-12 16:06:34 +0800

    更新项目名称设置

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,13 @@ import Demo from '../views/demo'
 
 const routes = [
   {
-    path: '/',
+    path: '/demo',
     name: 'demo',
     component: Demo
   },
   {
     path: '/hello',
     name: 'Hello',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "hello" */ '../components/HelloWorld')
   }
 ]
